791b6f7213375f1ed51d2547b8253bf5b9987cb9
suresh <sureshkumar.n@knila.com>
2021-12-16 19:23:32 +0530

    commit:First Commit

--- app/src/main/java/com/library/kotlinlmscustomertry/login/LoginActivity.kt
+++ app/src/main/java/com/library/kotlinlmscustomertry/login/LoginActivity.kt
@@ -1,12 +1,33 @@
 package com.library.kotlinlmscustomertry.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.example.kotlinmvvmsample.utils.showToast
 import com.library.kotlinlmscustomertry.R
+import com.library.kotlinlmscustomertry.databinding.ActivityLoginBinding
+import com.library.kotlinlmscustomertry.home.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var viewDataBinding: ActivityLoginBinding
+    val viewModel:LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewDataBinding=DataBindingUtil.setContentView(this,R.layout.activity_login)
+        viewDataBinding.loginModel=viewModel
+        viewDataBinding.lifecycleOwner=this
+        setContentView(viewDataBinding.root)
+        init()
+    }
+    private fun init(){
+        viewModel.errorAlret.observe(this,{
+            if(it.isNotEmpty()){
+                showToast(it)
+            }else
+                startActivity(Intent(this, HomeActivity::class.java))
+        })
+
     }
 }
--- app/src/main/java/com/library/kotlinlmscustomertry/login/LoginViewModel.kt
+++ app/src/main/java/com/library/kotlinlmscustomertry/login/LoginViewModel.kt
@@ -3,8 +3,33 @@ package com.library.kotlinlmscustomertry.login
 import android.app.Application
 import android.content.Context
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import com.example.kotlinmvvmsample.utils.AppController
+import com.example.kotlinmvvmsample.utils.checkTrim
 
 class LoginViewModel(application:Application):AndroidViewModel(application) {
     val Context=application as AppController
+    val memberIdTxt=MutableLiveData<String>("")
+    val passwordTxt=MutableLiveData<String>("")
+    val isChecked=MutableLiveData<Boolean>(false)
+    val errorAlret=MutableLiveData<String>("")
+
+    fun onLogin(){
+
+        val strMemberId=memberIdTxt.value!!.checkTrim()
+        val strPassword=passwordTxt.value!!.trim()
+
+        when {
+            strMemberId.isEmpty() -> {
+                errorAlret.value="Enter MemberId"
+            }
+            strPassword.isEmpty() -> {
+                errorAlret.value="Enter Password"
+            }else->{
+                errorAlret.value=""
+            }
+        }
+
+
+    }
 }
